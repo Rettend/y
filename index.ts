3e230d166b7ce500c65729e1915141a2b9ff529b
Hegyi Áron Ferenc <36823200+Rettend@users.noreply.github.com>
2024-03-29 17:12:55 +0100

    wtf

--- index.ts
+++ index.ts
@@ -1,8 +1,10 @@
 import process from 'node:process'
 import { URL } from 'node:url'
+import * as http from 'node:http'
 import { Client, GatewayIntentBits } from 'discord.js'
 import { loadCommands, loadEvents } from './src/util/loaders'
 import { registerEvents } from './src/util/registerEvents'
+import { log } from './src/util/utils'
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] })
 
@@ -16,3 +18,13 @@ const commands = await loadCommands(r('commands/'))
 registerEvents(commands, events, client)
 
 client.login(process.env.DISCORD_TOKEN)
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200
+  res.setHeader('Content-Type', 'text/plain')
+  res.end('Hello World\n')
+})
+
+server.listen(3000, '0.0.0.0', () => {
+  log('SERVER', `Server running at http://0.0.0.0:3000/`)
+})
