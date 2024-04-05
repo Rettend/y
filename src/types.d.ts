5699b027301e3b306d7cb335b74d2318f6b3007a
Hegyi Áron Ferenc <36823200+Rettend@users.noreply.github.com>
2024-04-05 21:20:08 +0200

    chore: cleanup

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,11 +1,9 @@
-declare global {
-  namespace NodeJS {
-    interface ProcessEnv {
-      APPLICATION_ID: string | undefined
-      DISCORD_TOKEN: string | undefined
-      OPENAI_API_KEY: string | undefined
-      SYSTEM_PROMPT: string | undefined
-    }
+declare module 'bun' {
+  interface Env {
+    APPLICATION_ID: string | undefined
+    DISCORD_TOKEN: string | undefined
+    OPENAI_API_KEY: string | undefined
+    SYSTEM_PROMPT: string | undefined
   }
 }
 
